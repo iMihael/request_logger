670ff379db45b6cd6845905cd0c42e6e1ba3c4c1
mike <mike@zeoalliance.com>
2016-09-23 13:22:08 +0300

    implement tags

--- app/WebHook.php
+++ app/WebHook.php
@@ -21,22 +21,80 @@ class WebHook
     private function commands()
     {
         return [
-            '/^\/start$/' => 'start'
+            '/^\/start$/' => 'start',
+            '/^\/subscribe ([a-zA-Z0-9]+)$/' => 'subscribe',
+            '/^\/unsubscribe ([a-zA-Z0-9]+)$/' => 'unsubscribe',
         ];
     }
 
     private function parse()
     {
         if($body = json_decode($this->request->getContent(), true)) {
+
+            $collection = $this->db->selectCollection('raw_webhook');
+            $collection->insertOne($body);
+
             if(isset($body['message']) && isset($body['message']['text'])) {
                 $message = $body['message']['text'];
                 foreach($this->commands() as $command => $method) {
-                    if(preg_match($command, $message)) {
-                        $this->$method($body);
-                        break;
+                    $matches = [];
+                    if(preg_match($command, $message, $matches)) {
+                        if(method_exists($this, $method)) {
+                            $this->$method($body, $matches);
+                        }
+                        return;
                     }
                 }
             }
+        }
+    }
+
+    public function subscribe($body, $matches)
+    {
+        $collection = $this->db->selectCollection('user');
+        if($user = $collection->findOne(['user_id' => $body['message']['from']['id']])) {
+            $tags = isset($user['tags']) ? (array)$user['tags'] : [];
+            $tag = trim($matches[1]);
+            if(!in_array($tag, $tags)) {
+                $tags[] = $tag;
+
+                $collection->updateOne(['user_id' => $body['message']['from']['id']], [
+                    '$set' => [
+                        'tags' => $tags,
+                    ]
+                ]);
+            }
+
+
+            (new Telegram())->sendRequest('sendMessage', [
+                'chat_id' => $body['message']['chat']['id'],
+                'text' => "You are subscribed to tag " . $tag,
+            ]);
+        }
+    }
+
+    public function unsubscribe($body, $matches)
+    {
+        $collection = $this->db->selectCollection('user');
+        if($user = $collection->findOne(['user_id' => $body['message']['from']['id']])) {
+            $tags = isset($user['tags']) ? (array)$user['tags'] : [];
+            $tag = trim($matches[1]);
+            if(in_array($tag, $tags)) {
+                unset($tags[array_search($tag, $tags)]);
+                $tags = array_values($tags);
+
+                $collection->updateOne(['user_id' => $body['message']['from']['id']], [
+                    '$set' => [
+                        'tags' => $tags,
+                    ]
+                ]);
+            }
+
+
+            (new Telegram())->sendRequest('sendMessage', [
+                'chat_id' => $body['message']['chat']['id'],
+                'text' => "You are subscribed from tag " . $tag,
+            ]);
         }
     }
 
